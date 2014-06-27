bf91afbc916c9d9dca1c508fb449c58e0e3990f9
Swaff <swaff68@gmail.com>
2014-06-26 19:04:38 -0600

    finished

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
 $(document).on('ready', function() {
   
+
+	var $el;
+
+	$('.button').click(function() {
+		$el = $(this);
+		$el.hide().after('<textarea></textarea>');
+	});
+
+	$('body').on('blur', 'textarea', function() {
+		$(this).hide();
+		$el.show().html($(this).val());
+	});
+
+
+	var $el;
+
+	$('.editable').click(function() {
+		$el = $(this);
+		$el.hide().after('<textarea></textarea>');
+	});
+
+	$('body').on('blur', 'textarea', function() {
+		$(this).hide();
+		$el.show().html($(this).val());
+	});
+
 });
